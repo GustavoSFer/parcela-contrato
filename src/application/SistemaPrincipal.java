421032e551c8121d5eb21ad416042f0c12a1b626
GustavoSFer <gustavo.sfernander@gmail.com>
2023-10-20 22:59:31 -0300

    Criando nosso programa principal e solicitando as informaçoes do contrato

--- src/application/SistemaPrincipal.java
+++ src/application/SistemaPrincipal.java
@@ -1,10 +1,29 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Contrato;
+
 public class SistemaPrincipal {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
+    System.out.println("Entre com os dados da parcela");
+    System.out.print("Número do contrato: ");
+    int numero = sc.nextInt();
+    System.out.print("Data do contrato (dd/mm/aaaa): ");
+    LocalDate data = LocalDate.parse(sc.next(), dtf);
+    System.out.print("Valor do contrato: ");
+    double valor = sc.nextDouble();
+
+    Contrato contrato = new Contrato(numero, data, valor);
+
+    sc.close();
   }
 
 }
